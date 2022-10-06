5c08dae6ef87e085895385c0706ff346f7e1dbdf
kasulaarunteja <kasulaarunteja@gmail.com>
2022-10-06 14:28:02 +0530

    set

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+module.exports =()=>{
+    mongoose.connect("mongodb+srv://aruneja:arunteja123@cluster0.juemcu7.mongodb.net/?retryWrites=true&w=majority")
+}
+
+// const { default: mongoose } = require("mongoose");
+// require("dotenv").config();
+
+// const connect = async () => {
+// 	try {
+// 		return mongoose.connect(process.env.DB);
+// 	} catch (error) {
+// 		return console.log(error);
+// 	}
+// };
+
+// module.exports = { connect };
