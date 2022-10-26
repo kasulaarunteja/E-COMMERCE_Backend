0eb68a77fb863097c418296bb560331947882e8c
kasulaarunteja <kasulaarunteja@gmail.com>
2022-10-26 19:59:54 +0530

    user updata

--- src/controllers/crud.controller.js
+++ src/controllers/crud.controller.js
@@ -1,53 +1,53 @@
 const post = (model) => async (req, res) => {
   try {
-    const user_id = req.user._id;
+    const user_id = req.user._id
     const item = await model.create({
       title: req.body.title,
       price: req.body.price,
       user_id: user_id,
-    });
-    return res.status(201).send(item);
+    })
+    return res.status(201).send(item)
   } catch (err) {
-    return res.status(500).send({ Error: err.message });
+    return res.status(500).send({ Error: err.message })
   }
-};
+}
 
 const getAll = (model) => async (req, res) => {
   try {
-    const item = await model.find().lean().exec();
-    return res.send(item);
+    const item = await model.find().lean().exec()
+    return res.send(item)
   } catch (err) {
-    return res.status(500).send({ Error: err.message });
+    return res.status(500).send({ Error: err.message })
   }
-};
+}
 
 const getOne = (model) => async (req, res) => {
   try {
-    const item = await model.find({ id: req.params.id }).lean().exec();
-    return res.send(item);
+    const item = await model.find({ id: req.params.id }).lean().exec()
+    return res.send(item)
   } catch (err) {
-    return res.status(500).send({ Error: err.message });
+    return res.status(500).send({ Error: err.message })
   }
-};
+}
 const Delete = (model) => async (req, res) => {
   try {
-    const item = await model.findByIdAndDelete(req.params.id).lean().exec();
-    return res.send(item);
+    const item = await model.findByIdAndDelete(req.params.id).lean().exec()
+    return res.send(item)
   } catch (er) {
-    return res.status(500).send({ Error: er.message });
+    return res.status(500).send({ Error: er.message })
   }
-};
+}
 const patch = (model) => async (req, res) => {
   try {
     const item = await model
       .findByIdAndUpdate(req.params.id, req.body, { new: true })
       .lean()
-      .exec();
-    return res.send(item);
+      .exec()
+    return res.send(item)
   } catch (er) {
-    return res.status(500).send({ Error: er.message });
+    return res.status(500).send({ Error: er.message })
   }
-};
+}
 
 module.exports = (model) => {
   return {
@@ -56,5 +56,5 @@ module.exports = (model) => {
     getOne: getOne(model),
     delete: Delete(model),
     patch: patch(model),
-  };
-};
+  }
+}
